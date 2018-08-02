c2b4725c6c77c6225d3d997e2e3c9ea51e266775
cgad <35946778+cgad@users.noreply.github.com>
2018-08-02 10:02:26 -0600

    testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,46 +9,58 @@ var wordChoices = ["banana","passionfruit","orange","mango"];
 var randomWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 console.log(randomWord);
 
+
+// GAME SETUP
+// uses the random word to fill blankWord array with blank spaces
 function blankWordSetUp() {
     for (var i = 0; i < randomWord.length; i++) {
-        currentWord = blankWord.push("_ ");
+        blankWord.push("_ ");
     }
-    // document.getElementById("currentWord").textContent = currentWord;
+    console.log(blankWord);
+    document.getElementById("currentWord").textContent = blankWord;
 }
-
 blankWordSetUp();
 
+
+// GAME START
+// still need: letters already guessed to not update if letter already guessed
 document.onkeyup = function(event) {
     var userGuess = event.key.toLowerCase();
 
-    for (var i = 0; i < randomWord.length; i++) {
-        if (userGuess === randomWord[i]) {
-            console.log("yes, in position " + i);
-            blankWord.splice(i,1,randomWord[i]);
-            console.log(blankWord);
-        } 
-        // if (userGuess != randomWord[i]) {
-        //     lettersGuessed.push(userGuess);
-        //     console.log("letters guessed: " + lettersGuessed);
-        // }
+    // if the user's guess is unique
+    if (lettersGuessed.indexOf(userGuess) == -1) {
+
+        // determines if the letter guessed is in the random word
+        // if yes, replaces corresponding blank space in blankWord with the guessed letter
+        for (var i = 0; i < randomWord.length; i++) {
+            if (userGuess === randomWord[i]) {
+                console.log("yes, in position " + i);
+                blankWord.splice(i,1,randomWord[i]);
+                console.log(blankWord);
+            } 
+        }
+
+        // FIX: can't print an array to the screen without the commas, instead have to loop through
+        // document.getElementById("currentWord").textContent = blankWord;
+
+        guessesLeft--;
+        lettersGuessed.push(userGuess);
+
+        if (guessesLeft == 0) {
+            losses++;
+            guessesLeft = 12;
+            lettersGuessed = [];
+        }
+
+        console.log("letters guessed: " + lettersGuessed);
+        console.log("guesses left: " + guessesLeft);
+        console.log("losses: " + losses);
+        console.log(blankWord);
+
+        document.getElementById("wins").textContent = wins;
+        document.getElementById("losses").textContent = losses;
+        document.getElementById("guessesLeft").textContent = guessesLeft;
+        document.getElementById("lettersGuessed").textContent = lettersGuessed;
+
     }
-    guessesLeft--;
-    lettersGuessed.push(userGuess);
-
-    if (guessesLeft == 0) {
-        losses++;
-        guessesLeft = 12;
-        lettersGuessed = [];
-    }
-
-    console.log("letters guessed: " + lettersGuessed);
-    console.log("guesses left: " + guessesLeft);
-    console.log("losses: " + losses);
-    console.log(blankWord);
-
-    document.getElementById("wins").textContent = wins;
-    document.getElementById("losses").textContent = losses;
-    document.getElementById("guessesLeft").textContent = guessesLeft;
-    document.getElementById("lettersGuessed").textContent = lettersGuessed;
 }
-
