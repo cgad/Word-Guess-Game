04ce23873d03970586e8324a251e2abf8b50f5b7
cgad <35946778+cgad@users.noreply.github.com>
2018-08-01 22:24:32 -0600

    consolidate code and testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,52 @@
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
+var guessesLeft = 12;
 var lettersGuessed = [];
 var blankWord = [];
 
 var wordChoices = ["banana","passionfruit","orange","mango"];
 
+var randomWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+console.log(randomWord);
+
+function blankWordSetUp() {
+    for (var i = 0; i < randomWord.length; i++) {
+        blankWord.push("_ ");
+    }
+}
+
+blankWordSetUp();
+
 document.onkeyup = function(event) {
     var userGuess = event.key.toLowerCase();
-    var randomWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-    console.log(randomWord);
-    var numberLetters = randomWord.length;
-    var blankWord = [];
 
     for (var i = 0; i < randomWord.length; i++) {
-        if (userGuess == randomWord[i]) {
-            blankWord.splice(i, 0, userGuess);
+        if (userGuess === randomWord[i]) {
+            console.log("yes, in position " + i);
+            blankWord.splice(i,1,randomWord[i]);
             console.log(blankWord);
-        }
+        } 
+        // if (userGuess != randomWord[i]) {
+        //     lettersGuessed.push(userGuess);
+        //     console.log("letters guessed: " + lettersGuessed);
+        // }
     }
-    
-    for (var i = 0; i < randomWord.length; i++)
-        if (userGuess != randomWord[i]) {
-            guessesLeft--;
-            lettersGuessed.push(userGuess);
-            console.log(lettersGuessed);
-        }
+    guessesLeft--;
+    lettersGuessed.push(userGuess);
 
-    // if (guessesLeft = 0) {
-    //     losses++;
-    //     lettersGuessed = [];
-    // }
+    console.log("letters guessed: " + lettersGuessed);
+    console.log("guesses left: " + guessesLeft);
+    console.log("losses: " + losses);
 
-
-    // for (var i = 0; i <= numberLetters; i++) {
-    //     if (userGuess == randomWord[i]) {
-    //         blankWord.push(userGuess);
-    //         console.log(blankWord);
+    // for (var j = 0; j < lettersGuessed.length; j++) {
+    //     if (userGuess != lettersGuessed[j]) {
+    //         lettersGuessed.push(userGuess);
+    //         console.log("letters guessed: " + lettersGuessed);
     //     }
     // }
-
-    // if () {
-    //     guessesLeft--;
-    //     lettersGuessed.push(userGuess);
-    // }
-
-    document.getElementById("wins").textContent = wins;
-    document.getElementById("losses").textContent = losses;
-    document.getElementById("guessesLeft").textContent = guessesLeft;
-    document.getElementById("lettersGuessed").textContent = lettersGuessed;
 }
+
+    // document.getElementById("wins").textContent = wins;
+    // document.getElementById("losses").textContent = losses;
+    // document.getElementById("guessesLeft").textContent = guessesLeft;
+    // document.getElementById("lettersGuessed").textContent = lettersGuessed;
