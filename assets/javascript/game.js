789fa01bdcffc2d986af944d9709f2e8402804b4
cgad <35946778+cgad@users.noreply.github.com>
2018-08-02 21:22:55 -0600

    change styling, add reset.css content, edit verbage

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,27 +4,28 @@ var guessesLeft = 12;
 var lettersGuessed = [];
 var blankWord = [];
 var holder = [];
-var wordChoices = ["banana","passionfruit","orange","mango"];
-
-// var randomWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-// console.log(randomWord);
+var wordChoices = ["banana","passionfruit","orange","mango","grapefruit","kiwi"];
 
 
-// GAME SETUP
+// FUNCTIONS
+
+// randomly choose word from wordChoices
+function wordGen() {
+    randomWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+}
+
+// initial game setup
 // uses the random word to fill blankWord array with blank spaces
 function setUp() {
     wordGen();
+    document.getElementById("currentWordLength").textContent = randomWord.length;
     for (var i = 0; i < randomWord.length; i++) {
         blankWord.push("_");
     }
     document.getElementById("currentWord").textContent = blankWord.join(" ");
 }
 
-function wordGen() {
-    randomWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-    // console.log(randomWord);
-}
-
+// reset game to initial state
 function reSet() {
     lettersGuessed = [];
     holder = [];
@@ -34,11 +35,12 @@ function reSet() {
     setUp();
 }
 
-// INITIAL SETUP
+
+// SET UP PAGE
 setUp();
 
+
 // GAME START
-// still need: letters already guessed to not update if letter already guessed
 document.onkeyup = function(event) {
     var userGuess = event.key.toLowerCase();
 
@@ -46,20 +48,18 @@ document.onkeyup = function(event) {
     // if yes, replaces corresponding blank space in blankWord with the guessed letter
     for (var i = 0; i < randomWord.length; i++) {
         if (userGuess === randomWord[i]) {
-            // console.log("yes, in position " + i);
             blankWord.splice(i,1,randomWord[i]);
             holder.push(randomWord[i]);
             if (holder.length == randomWord.length) {
                 wins++;
                 reSet();
             }
-            // console.log(blankWord);
             document.getElementById("currentWord").textContent = blankWord.join(" ")
         } 
     }
 
     // if the user's guess is unique
-    if (lettersGuessed.indexOf(userGuess) == -1) {
+    if (lettersGuessed.indexOf(userGuess) == -1 && holder.indexOf(userGuess) == -1) {
         lettersGuessed.push(userGuess);
         guessesLeft--;
     }
@@ -70,14 +70,9 @@ document.onkeyup = function(event) {
         reSet();
     }
 
-    // console.log("letters guessed: " + lettersGuessed);
-    // console.log("guesses left: " + guessesLeft);
-    // console.log("losses: " + losses);
-    // console.log(blankWord);
-
     document.getElementById("wins").textContent = wins;
     document.getElementById("losses").textContent = losses;
     document.getElementById("guessesLeft").textContent = guessesLeft;
-    document.getElementById("lettersGuessed").textContent = lettersGuessed;
+    document.getElementById("lettersGuessed").textContent = lettersGuessed.join(" ");
 
 }
